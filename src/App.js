edc149b6b73fed9298ac8331dd7da43261f89cd1
SLuo5991 <sluo5991@gmail.com>
2022-08-17 16:21:09 -0400

    added portfolio

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import './style.css';
+// named export
+import { Card } from './components/Card';
+
 function App() {
   return (
-    <div className="App">
-
+    <div className='App'>
+      <Card />
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,10 @@
+import portfolio from '../assets/images/1.jpg';
+
+export function Card() {
+  return (
+    <div>
+      <img src={portfolio} alt='portfolio' />
+    </div>
+  );
+}
+
