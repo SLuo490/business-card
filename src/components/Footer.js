d47e1f77cbdda0d6ec89fb7f35b6d98efd9ab658
SLuo5991 <sluo5991@gmail.com>
2022-08-25 10:11:16 -0400

    add icons to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,43 @@
 import '../style.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faTwitter,
+  faFacebook,
+  faInstagram,
+  faLinkedinIn,
+  faGithub,
+} from '@fortawesome/free-brands-svg-icons';
 
 export default function Footer() {
   return (
     <div className='footer-container'>
-      <h1>Footer</h1>
+      <ul>
+        <li>
+          <a href='https://twitter.com/'>
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
+        </li>
+        <li>
+          <a href='https://fb.com/'>
+            <FontAwesomeIcon icon={faFacebook} />
+          </a>
+        </li>
+        <li>
+          <a href='https://instagram.com/'>
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+        </li>
+        <li>
+          <a href='https://linkedin.com/'>
+            <FontAwesomeIcon icon={faLinkedinIn} />
+          </a>
+        </li>
+        <li>
+          <a href='https://github.com/'>
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
